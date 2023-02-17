6f4f2a6ea30937a3401b3bfe1772a05cfb297b85
Amy Richardson <amyloumc@gmail.com>
2023-02-17 19:50:06 +0000

    Add comments for scope and creds

--- run.py
+++ run.py
@@ -1,14 +1,22 @@
-# Settings to access spreadsheet data from Google Sheets
+# Settings and credentials to allow access, read and modify data in
+# Google Sheets
 import gspread
 from google.oauth2.service_account import Credentials
 
+
+# Scope for Google IAM auth for API program access
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
     ]
 
+
+# const for untracked creds file
 CREDS = Credentials.from_service_account_file('creds.json')
+# const for credentials scope
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+# const to allow authorisation of gspread client within these scoped credentials
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+# const for google sgeet
 SHEET = GSPREAD_CLIENT.open('bakestock')
